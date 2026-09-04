77d318f906e1cb6d6b88c43a31296833e58c5c45
VamsI-261203 <kalivarapuvamsi123@gmail.com>
2026-09-04 21:44:57 +0530

    Added singlelinkedlist-addfirst,addlast,deletefirst,deletelast,addarindex

--- programs/maths/linkedList.java
+++ programs/maths/linkedList.java
@@ -1,137 +1,77 @@
-// public class linkedList {
-
-//     public static void main(String[] args) {
-
-//         // Create a linked list object
-//         linkedList list = new linkedList();
-
-//         // Insert elements at the beginning
-//         list.insertFirst(10);
-//         list.insertFirst(20);
-//         list.insertFirst(30);
-//         list.insertFirst(40);
-//         list.insertFirst(50);
-
-//         // Display the linked list
-//         list.display();
-//     }
-
-//     // Head points to the first node
-//     private Node head;
-
-//     // Tail points to the last node
-//     private Node tail;
-
-//     // Stores the number of nodes
-//     private int size;
-
-
-//     // Constructor
-//     public linkedList() {
-//         this.size = 0;
-//     }
-
-
-//     // Insert a new node at the beginning
-//     public void insertFirst(int val) {
-
-//         // Create a new node
-//         Node node = new Node(val);
-
-//         // New node points to the current head
-//         node.next = head;
-
-//         // Make the new node the head
-//         head = node;
-
-//         // If this is the first node,
-//         // tail should also point to this node
-//         if (tail == null) {
-//             tail = head;
-//         }
-
-//         // Increase the size
-//         size++;
-//     }
-
-
-//     // Display all elements
-//     public void display() {
-
-//         // Start from the head
-//         Node temp = head;
-
-//         // Continue until we reach null
-//         while (temp != null) {
-
-//             System.out.print(temp.value + " --> ");
-
-//             // Move to the next node
-//             temp = temp.next;
-//         }
-
-//         System.out.println("END");
-//     }
-
-
-//     // Node class
-//     private class Node {
-
-//         // Stores the data
-//         private int value;
-
-//         // Stores the reference to the next node
-//         private Node next;
-
-
-//         // Node constructor
-//         private Node(int value) {
-//             this.value = value;
-//         }
-//     }
-// }
-public class linkedList{
-    public static void main(String[] args){
-        linkedList list =new linkedList();
-        list.insertFirst(10);
-        list.insertFirst(20);
-        list.insertFirst(30);
-        list.insertFirst(40);
-        list.insertFirst(50);
-        list.display();
-    }
-    private Node head;
-    private Node tail;
-    private int size;
-    public linkedList(){
-        this.size=0;
-    }
-    public void insertFirst(int val){
-        Node node=new Node(val);
-        node.next=head;
-        head=node;
-        if(tail==null){
-            tail=head;
+class LinkedList{
+	class Node{
+		String data;
+		Node next;
+		public Node(String data){
+			this.data=data;
+			this.next=null;
+			}
+}
+        Node head;
+		public void addFirst(String data){
+				Node newNode=new Node(data);
+				if(head==null){
+					head=newNode;
+					}
+				else{
+					newNode.next=head;
+					head=newNode;
+					}
+		}
+		public void addLast(String data){
+			Node newNode=new Node(data);
+			if(head==null){
+				head=newNode;
+				return;
+				}
+				Node currNode=head;
+				while(currNode.next!=null){
+					currNode=currNode.next;
+					}
+					currNode.next=newNode;
+				}
+        public void deleteFirst(){
+            if(head==null){
+                System.out.println("List is empty");
+                return;
+            }
+            head=head.next;
         }
-        size++;
-    }
-    public void display(){
-        Node temp=head;
-        while(temp!=null){
-            System.out.print(temp.val+"-->");
-            temp=temp.next;
+        public void deleteLast(){
+            if(head==null){
+                System.out.println("List is Empty");
+                return;
+            }
+            if(head.next==null){
+                head=null;
+                return;
+            }
+            Node secondLast=head;
+            Node lastNode=head.next;
+            while(lastNode.next!=null){
+                secondLast=secondLast.next;
+                lastNode=lastNode.next;
+            }
+            secondLast.next=null;
         }
-        System.out.println("END");
-    }
-    private class Node{
-        private int val;
-        private Node next;
-        private  Node(int val){
-            this.val=val;
-        }
-         private  Node(int val,Node next){
-            this.val=val;
-            this.next=next;
-        }
+		void display(){
+			if(head==null){
+				System.out.println("List is empty");
+				return;
+			}
+			Node currNode=head;
+			while(currNode!=null){
+				System.out.print(currNode.data+"-->");
+				currNode=currNode.next;
+				}
+                			System.out.println("null");
+			}
+
+	public static void main(String[] args){
+			LinkedList list=new LinkedList();
+			list.addFirst("Is");
+			list.addLast("a");
+			list.addFirst("List");
+            list.display();
     }
 }
